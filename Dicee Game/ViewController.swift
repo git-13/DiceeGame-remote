34b06f2a2927b8aaf8800868933de3537e7d0a6c
Vladislav <git13vl@gmail.com>
2022-07-08 20:44:44 +0200

    Initial Commit

---
+++ Dicee Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dicee Game
+//
+//  Created by Vladislav Glovatskiy on 08.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
